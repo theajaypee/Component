e5e2cb6e232f33e0510a0efdda106312aee78a69
theajaypee <110385542+theajaypee@users.noreply.github.com>
2022-08-08 20:55:00 +0800

    javascript

---
+++ main.js
@@ -0,0 +1,10 @@
+var seconddiv = document.getElementById('orangediv')
+
+// poping an alert when clicked
+seconddiv.addEventListener('click', function(){
+    alert('orange div is clicked')
+})
+
+
+// changing the element background
+seconddiv.style.background = "blue"
