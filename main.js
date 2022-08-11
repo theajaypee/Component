5550c3acdd3ee57559a4b81f54c3b3f9be5664f2
theajaypee <110385542+theajaypee@users.noreply.github.com>
2022-08-11 17:04:26 +0800

    java update

--- main.js
+++ main.js
@@ -1,10 +1,67 @@
-var seconddiv = document.getElementById('orangediv')
+// var seconddiv = document.getElementById('orangediv')
 
 // poping an alert when clicked
-seconddiv.addEventListener('click', function(){
-    alert('orange div is clicked')
-})
+// seconddiv.addEventListener('click', function(){
+    // alert('orange div is clicked')
+// })
 
 
 // changing the element background
-seconddiv.style.background = "blue"
+// seconddiv.style.background = "blue"
+
+
+// console.log('welcome, thejay, this works')
+
+// console.log(document)
+
+// alert('Welcome to my first day with javascript')
+
+// document.querySelector(".right-holder").innerHTML = "supper cool it works"
+
+// function poosomething() {
+    // alert('Welcome, box 1 is clicked')
+// }
+
+var rightdiv = document.querySelector('#right-holder')
+// console.log(rightdiv)
+
+
+// function changetoorange() {
+//     rightdiv.style.background = '#ffa800'
+
+// }
+// function changetochocolate() {
+//     rightdiv.style.background = 'chocolate'
+
+// }
+// function changetoskyblue() {
+//     rightdiv.style.background = 'skyblue'
+
+// }
+
+function printsomething(yourwriteup){
+    console.log(yourwriteup)
+}
+printsomething('what my name')
+printsomething('call my name')
+printsomething('who is your daddy')
+
+
+
+
+
+function ChangeColor(particularcolor) {
+    rightdiv.style.background = particularcolor
+}
+
+
+
+
+var hamburgermenuholder = document.querySelector('.menubar-holder')
+
+console.log(hamburgermenuholder)
+
+
+function changeclass() {
+    hamburgermenuholder.classList.toggle('purplemenu')
+}
